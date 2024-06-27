d49341ad1eea480f298c44f95f9c3113df84c8f8
Welkani <baptiti54@gmail.com>
2024-06-27 16:33:35 +0200

    deleted dupilcates

--- database/migrations/2024_06_27_122937_create_inscription_table.php
+++ database/migrations/2024_06_27_122937_create_inscription_table.php
@@ -11,31 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        
-        Schema::create('eleve', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('prenom',70);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            
-        
-        });
-        Schema::create('cours', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('professeur');
-            
-            
-        });
         Schema::create('inscription', function (Blueprint $table) {
-            $table->dateTime('created_at')->useCurrent();
-            $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
+           
             $table->foreignId('cours_id')->constrained('cours')->onDelete('cascade');
             $table->foreignId('eleve_id')->constrained('users')->onDelete('cascade');
-            $table->timestamps();
+            //$table->timestamps();
             $table->dateTime('created_at')->useCurrent();
             $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
